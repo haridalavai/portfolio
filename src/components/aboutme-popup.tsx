d2ebc5dae9ae89c8aaf172957924313c16e999ea
haridalavai <dalavai.hari@gmail.com>
2024-08-02 10:11:09 +0530

    add about me

--- src/components/aboutme-popup.tsx
+++ src/components/aboutme-popup.tsx
@@ -17,7 +17,7 @@ import remarkGfm from "remark-gfm";
 
 const markdownContent = `
 ![Hari](https://haridalavai.me/me.jpg) 
-Hey! I’m Hari. Thanks for checking out my portfolio. I’m Srihari, a software engineer with 3 years of experience. Most of the time, you can find me coding 💻, painting 🎨, playing games 🎮, cooking up a storm 🍳, reading like a bookworm 📚, binge-watching anime 📺, overthinking everything 🤔, or sometimes going on treks because I thought it was a good idea—until my legs remind me otherwise 🥲.
+Hey! I’m Hari. Thanks for checking out my portfolio. I’m Srihari, a software engineer with 3 years of experience. Most of the time, you can find me coding 💻, painting 🎨, playing games 🎮, cooking up a storm 🍳, reading anything from science fiction to shampoo bottles 📚, binge-watching anime 📺, overthinking everything 🤔, or sometimes going on treks-because who doesn't love getting their legs hurt for fun? 🥾.
 
 If you like science fiction, fantasy, or anime, we’re destined to be friends! I’m always dreaming up problems and solutions, or figuring out how to make good solutions even better. My latest brainchild is Xcopilot—imagine Siri, but for SaaS products. It lets users chat with the product and magically turns their queries into actionable tasks. 🚀 So, if you’re ready for some tech wizardry, let’s connect!`;
 
@@ -36,11 +36,11 @@ const AboutMePopup = () => {
           )}
         </Marquee>
       </DialogTrigger>
-      <DialogContent className=" fixed top-0 w-full h-screen pt-16">
+      <DialogContent className=" fixed top-0 w-full h-screen pt-16 overflow-auto">
         <DialogHeader className="border border-gray-500 p-3">
           <DialogTitle>About Me</DialogTitle>
         </DialogHeader>
-        <p className="border border-gray-500 border-t-0 p-3">
+        <p className="border border-gray-500 border-t-0 p-3 ">
           Content goes here
           <Markdown remarkPlugins={[remarkGfm]}>{markdownContent}</Markdown>
         </p>
